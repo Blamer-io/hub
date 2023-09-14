c4a6bd2e5e8f20b8b175ffe2db7004e96d27072d
Igor <igor1996popovich@gmail.com>
2023-09-14 21:47:01 +0300

    issue-52 14.09.2023

--- src/main/java/io/blamer/hub/controller/TokenController.java
+++ src/main/java/io/blamer/hub/controller/TokenController.java
@@ -3,11 +3,15 @@ package io.blamer.hub.controller;
 import io.blamer.hub.model.Tokens;
 import io.blamer.hub.rq.RequestToken;
 import io.blamer.hub.rq.TokenToAdd;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import static io.netty.handler.codec.http.HttpHeaderValidationUtil.validateToken;
 
 /**
  * Token controller.
@@ -41,8 +45,23 @@ public class TokenController {
    * @throws Exception if something went wrong
    */
   @PostMapping
-  Mono<Void> registerToken(@RequestBody final RequestToken request)
-    throws Exception {
-    return this.tokens.add(new TokenToAdd(request).value());
+  Mono<Void> registerToken(@RequestBody final RequestToken request) throws Exception {
+    String token = request.getValue();
+    String secretKey = "your-secret-key";
+
+    if (validateToken(token, secretKey)) {
+      return this.tokens.add(new TokenToAdd(request).value());
+    } else {
+      throw new RuntimeException("Invalid token");
+    }
+  }
+
+  public boolean validateToken(String token, String secretKey) {
+    try {
+      Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+      return true;
+    } catch (SignatureException e) {
+      return false;
+    }
   }
 }
